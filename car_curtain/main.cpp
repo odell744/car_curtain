10967c86e01ed22c01459a53ffc7dabbb78b7d17
odell744 <odell744@gmail.com>
2022-10-06 21:28:17 +0700

    update
    startup work

---
+++ car_curtain/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * car_curtain.cpp
+ *
+ * Created: 06.10.2022 21:24:21
+ * Author : odell
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
